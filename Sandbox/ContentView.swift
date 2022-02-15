7fa1bcbcd2e600f2ed44eb8c3a39311dbbd933d9
Giovanni Gaffe <gaffejonathan@ymail.com>
2022-02-15 11:32:20 +0800

    remove code in separate file

--- Sandbox/ContentView.swift
+++ Sandbox/ContentView.swift
@@ -7,34 +7,12 @@
 
 import SwiftUI
 
-struct User: Identifiable {
-    let id = UUID()
-    var name: String
-    var isContacted = false
-}
 
 struct ContentView: View {
-    @State private var users = [
-        User(name: "Taylor"),
-        User(name: "Justin"),
-        User(name: "Adele")
-    ]
-    
-    @State private var isOn = false
     var body: some View {
-        List($users) { $user in
-            HStack {
-                Text(user.name)
-                Spacer()
-                Toggle("User has been contacted",isOn: $user.isContacted)
-                    .labelsHidden()
-            }
-        }
-        //        Toggle("Example", isOn: $isOn)
-        //            .toggleStyle(SwitchToggleStyle())
-        //            .padding()
+       Text("Hello")
+            
     }
-    
 }
 
 struct ContentView_Previews: PreviewProvider {
