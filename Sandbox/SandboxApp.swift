90afaaf22dc999f3043c484305bbc1a945547e56
Giovanni Gaffe <gaffejonathan@ymail.com>
2022-02-15 09:57:46 +0800

    Initial Commit

---
+++ Sandbox/SandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandboxApp.swift
+//  Sandbox
+//
+//  Created by Giovanni Gaffé on 2022/2/15.
+//
+
+import SwiftUI
+
+@main
+struct SandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
