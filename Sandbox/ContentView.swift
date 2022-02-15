176bf2b952b5dc88bcc9ed4fb43ac7d3fc6c76df
Giovanni Gaffe <gaffejonathan@ymail.com>
2022-02-15 11:33:16 +0800

    add extension on URLSession

--- Sandbox/ContentView.swift
+++ Sandbox/ContentView.swift
@@ -7,6 +7,25 @@
 
 import SwiftUI
 
+extension URLSession {
+    func decode<T: Decodable>(
+        _ type: T.Type = T.self,
+        from url: URL,
+        keyDecodingStrategy: JSONDecoder.KeyDecodingStrategy = .useDefaultKeys,
+        dataDecodingStrategy: JSONDecoder.DataDecodingStrategy = .deferredToData,
+        dateDecodingStrategy: JSONDecoder.DateDecodingStrategy = .deferredToDate
+    ) throws -> T {
+        let (data, _) = try data(from: url)
+
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = keyDecodingStrategy
+        decoder.dataDecodingStrategy = dataDecodingStrategy
+        decoder.dateDecodingStrategy = dateDecodingStrategy
+
+        let decoded = try decoder.decode(T.self, from: data)
+        return decoded
+    }
+}
 
 struct ContentView: View {
     var body: some View {
