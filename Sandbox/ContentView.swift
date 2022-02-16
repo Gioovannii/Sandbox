07931391af993acba7f812c3542a23ac18b8bc37
Giovanni Gaffe <gaffejonathan@ymail.com>
2022-02-16 12:13:41 +0800

    remove code and put in async view

--- Sandbox/ContentView.swift
+++ Sandbox/ContentView.swift
@@ -7,55 +7,9 @@
 
 import SwiftUI
 
-extension URLSession {
-    func decode<T: Decodable>(
-        _ type: T.Type = T.self,
-        from url: URL,
-        keyDecodingStrategy: JSONDecoder.KeyDecodingStrategy = .useDefaultKeys,
-        dataDecodingStrategy: JSONDecoder.DataDecodingStrategy = .deferredToData,
-        dateDecodingStrategy: JSONDecoder.DateDecodingStrategy = .deferredToDate
-    ) async throws -> T {
-        let (data, _) = try await data(from: url)
-        
-        let decoder = JSONDecoder()
-        decoder.keyDecodingStrategy = keyDecodingStrategy
-        decoder.dataDecodingStrategy = dataDecodingStrategy
-        decoder.dateDecodingStrategy = dateDecodingStrategy
-        
-        let decoded = try decoder.decode(T.self, from: data)
-        return decoded
-    }
-}
-
-struct Message: Codable, Identifiable {
-    let id: Int
-    let user: String
-    let text: String
-}
-
 struct ContentView: View {
-    @State private var inbox = [Message]()
-    
     var body: some View {
-        NavigationView {
-            List(inbox) { message in
-                Text("\(message.user): ").bold() +
-                Text(message.text)
-            }
-            .navigationTitle("Inbox")
-            .task {
-                do {
-                    inbox = try await fetchInbox()
-                } catch {
-                    print(error.localizedDescription)
-                }
-            }
-        }        
-    }
-    
-    func fetchInbox() async throws -> [Message] {
-        let inboxURL = URL(string: "https://hws.dev/inbox.json")!
-        return try await URLSession.shared.decode(from: inboxURL)
+        Text("Hello world!")
     }
 }
 
