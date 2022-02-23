bf1eb71e0292df07dfecb76ec2eed6b62fc9caa2
Giovanni Gaffe <gaffejonathan@ymail.com>
2022-02-23 11:43:56 +0800

    set an extension for task

--- Sandbox/AsyncView.swift
+++ Sandbox/AsyncView.swift
@@ -7,6 +7,13 @@
 
 import SwiftUI
 
+
+extension Task where Success == Never, Failure == Never {
+    static func sleep(seconds: Double) async throws {
+        let duration = UInt64(seconds * 1_000_000_000)
+        try await sleep(nanoseconds: duration)
+    }
+}
 extension URLSession {
     func decode<T: Decodable>(
         _ type: T.Type = T.self,
